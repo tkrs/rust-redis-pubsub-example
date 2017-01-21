041f901ef2b690c2f415de11801e51be484dca7f
Takeru Sato <type.in.type@gmail.com>
2017-01-22 00:37:39 +0900

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+use std::thread;
+use std::time::Duration;
 extern crate redis;
 
-pub struct Ctx {
+struct Ctx {
     pub client: redis::Client,
 }
 
 impl Ctx {
-
     fn new() -> Ctx {
         let client = redis::Client::open("redis://localhost/").unwrap();
         Ctx {
@@ -17,42 +18,33 @@ impl Ctx {
     }
 }
 
-// fn fetch_an_integer() -> redis::RedisResult<i32> {
-//     let con = try!(client.get_connection());
-//     let _ : () = try!(con.set("my_key", 42));
-//     let my_val : i32 = try!(con.get("my_key"));
-//     Ok(my_val)
-// }
-//
-// fn recieve() -> redis::RedisResult<()> {
-//     let client = try!(redis::Client::open("redis://localhost/"));
-//     let mut pubsub = try!(client.get_pubsub());
-//     try!(pubsub.subscribe("channel_1"));
-//     try!(pubsub.subscribe("channel_2"));
-//
-//     loop {
-//         let msg = try!(pubsub.get_message());
-//         let payload : String = try!(msg.get_payload());
-//         println!("channel '{}': {}", msg.get_channel_name(), payload);
-//     }
-//
-// }
-
 fn main() {
-    // let r = fetch_an_integer();
-    // println!("{}", r.ok().unwrap());
-    // let _ : () = recieve()
-    let ctx = Ctx::new();
-    let mut ps = ctx.pubsub();
+    let handle = thread::spawn(|| {
+        let ctx = Ctx::new();
+        let mut ps = ctx.pubsub();
 
-    ps.subscribe("channel_1").unwrap();
-    ps.subscribe("channel_2").unwrap();
+        ps.subscribe("channel_1").unwrap();
+        ps.subscribe("channel_2").unwrap();
 
-    loop {
-        println!("loop...");
-        let msg = ps.get_message().unwrap();
-        let payload : String = msg.get_payload().unwrap();
-        println!("channel '{}': {}", msg.get_channel_name(), payload);
-    }
+        for x  in 0..10 {
+            println!("Subscriber: {} times...", x);
+            let msg = ps.get_message().unwrap();
+            let payload : String = msg.get_payload().unwrap();
+            println!("channel '{}': {}", msg.get_channel_name(), payload);
+        }
+    });
+
+    thread::sleep(Duration::from_millis(500));
+
+    thread::spawn(|| {
+        let ctx = Ctx::new();
+        let con = ctx.client.get_connection().unwrap();
+
+        for x  in 0..10 {
+            redis::cmd("PUBLISH").arg("channel_1").arg(x).execute(&con);
+        }
+    });
+
+    handle.join().unwrap();
 
 }
