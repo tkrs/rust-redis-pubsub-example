c2e65943dcca3da63433a889bd72c5a7072b6f8c
Takeru Sato <type.in.type@gmail.com>
2018-08-16 15:34:37 +0900

    Update to redis-rs 0.9

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,35 @@
+extern crate redis;
+
+use redis::{Client, ControlFlow, PubSubCommands};
 use std::thread;
 use std::time::Duration;
 
-extern crate redis;
-
 struct Ctx {
-    pub client: redis::Client,
+    pub client: Client,
 }
 
 impl Ctx {
     fn new() -> Ctx {
-        let client = redis::Client::open("redis://localhost/").unwrap();
+        let client = Client::open("redis://localhost/").unwrap();
         Ctx { client: client }
-    }
-    fn pubsub(&self) -> redis::PubSub {
-        self.client.get_pubsub().unwrap()
     }
 }
 
 fn subscribe(ctx: Ctx) -> thread::JoinHandle<()> {
     thread::spawn(move || {
-        let mut ps = ctx.pubsub();
+        let mut conn = ctx.client.get_connection().unwrap();
 
-        ps.subscribe("boo").unwrap();
-
-        println!("Subscriber is ready.");
-
-        loop {
-            let msg = ps.get_message().unwrap();
+        conn.subscribe(&["boo"], |msg| {
             let ch = msg.get_channel_name();
             let payload: String = msg.get_payload().unwrap();
             match payload.as_ref() {
-                "10" => break,
-                a => println!("Channel: '{}' received '{}'.", ch, a),
+                "10" => ControlFlow::Break(()),
+                a => {
+                    println!("Channel: '{}' received '{}'.", ch, a);
+                    ControlFlow::Continue
+                }
             }
-        }
+        }).unwrap();
     })
 }
 
@@ -42,10 +38,7 @@ fn publish(ctx: Ctx) {
         let con = ctx.client.get_connection().unwrap();
 
         for x in 0..11 {
-            redis::cmd("PUBLISH")
-                .arg("boo")
-                .arg(x)
-                .execute(&con);
+            redis::cmd("PUBLISH").arg("boo").arg(x).execute(&con);
         }
     });
 }
